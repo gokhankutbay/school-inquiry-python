a77ceb3dd9b678551c6e6a32407a779b5b680864
Gökhan Kutbay <gokhan200755@gmail.com>
2021-07-08 14:48:11 +0300

    Python File

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # school-inquiry-python
+
+### Don't forget to support me!
+
+[www.gokhankutbay.ml](https://www.gokhankutbay.ml/)
+
+### Follow me;
+
+[Instagram](http://www.instagram.com/gokhannkutbay/)\
+[Twitter](https://twitter.com/gokhankutbay0)\
+[GitHub](http://www.github.com/gokhankutbay/)\
+[LinkedIn](http://www.linkedin.com/in/gokhankutbay/)\
+[Facebook](https://www.facebook.com/gokhannkutbay)
---
+++ school-inquiry.py
@@ -0,0 +1,16 @@
+age = int(input("Enter the person's age:: "))
+
+if age<=5:
+    print("Nursery.")
+elif age==6:
+    print("Kindergarten student.")
+elif 7<=age<=10:
+    print("Primary school student.")
+elif 10<age<=14:
+    print("Middle school student.")
+elif 14<age<=18:
+    print("High school student.")
+elif age>18:
+    print("University student/Graduate.")
+
+# by GÖKHAN KUTBAY (www.gokhankutbay.ml)
